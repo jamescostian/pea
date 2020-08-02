3e8f3745fcd006540b1f54275a8401e76c706548
Jennings Zhang <jenni_zh@protonmail.com>
2020-08-02 09:22:04 -0400

    Remove status=progress

--- pea.sh
+++ pea.sh
@@ -3,6 +3,6 @@
 source $(which peat)
 
 set -x
-dd if=/dev/urandom of=$OTP bs=512 count=4 status=progress
+dd if=/dev/urandom of=$OTP bs=512 count=4
 cryptsetup luksAddKey $DISK $OTP
 systemctl stop pead
